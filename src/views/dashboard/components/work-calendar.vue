1af24cf72377920bde58036b9b673a3b4456b857
xiaoxiao <2683444623@qq.com>
2022-11-15 10:45:48 +0800

    完成主页审批模块

---
+++ src/views/dashboard/components/work-calendar.vue
@@ -0,0 +1,132 @@
+<template>
+  <div>
+    <!-- 工作日历 -->
+    <!-- 年和月 -->
+    <el-row type="flex" justify="end">
+      <el-select v-model="currentYear" size="small" style="width: 120px" @change="dateChange">
+        <el-option
+          v-for="item in yearList"
+          :key="item"
+          :label="item"
+          :value="item"
+        >
+          {{ item }}
+        </el-option>
+      </el-select>
+      <el-select v-model="currentMonth" size="small" style="width: 120px;margin-left:10px" @change="dateChange">
+        <el-option
+          v-for="item in 12"
+          :key="item"
+          :label="item"
+          :value="item"
+        >
+          {{ item }}
+        </el-option>
+      </el-select>
+    </el-row>
+    <!-- 日历组件 -->
+    <el-calendar v-model="currentDate">
+      <template v-slot:dateCell="{ date, data }" class="content">
+        <div class="date-content">
+          <span class="text"> {{ data.day | getDay }}</span>
+          <span v-if="isWeek(date)" class="rest">休</span>
+        </div>
+      </template>
+    </el-calendar>
+  </div>
+</template>
+
+<script>
+export default {
+  filters: {
+    getDay(value) {
+      const day = value.split('-')[2]
+      return day.startsWith('0') ? day.substr(1) : day
+    }
+  },
+  props: {
+    startDate: {
+      type: Date,
+      default: () => new Date()
+    }
+  },
+  data() {
+    return {
+      // 要遍历当前年份的数组
+      yearList: [],
+      // 当前年份
+      currentYear: null,
+      // 当前月份
+      currentMonth: null,
+      // 当前日期
+      currentDay: null,
+      // 当前时间
+      currentDate: null
+    }
+  },
+  created() {
+    // 获取当前年份
+    this.currentYear = this.startDate.getFullYear()
+    // 获取当前月份
+    this.currentMonth = this.startDate.getMonth() + 1
+    // 获取当前日期
+    this.currentDay = this.startDate.getDate()
+
+    // 快速生成数组方法
+    this.yearList = Array.from(Array(11), (v, i) => i + this.currentYear - 5)
+    // 计算 当年当月的第一个周一 再加上 四周之后的一个月月份
+    this.dateChange()
+  },
+  methods: {
+    // 年月份改变之后
+    dateChange() {
+      this.currentDate = new Date(`${this.currentYear}-${this.currentMonth}-${this.currentDay}`)
+    },
+    // 是否是休息日
+    isWeek(value) {
+      return value.getDay() === 6 || value.getDay() === 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+/deep/ .el-calendar-day {
+  height:  auto;
+ }
+/deep/ .el-calendar-table__row td,/deep/ .el-calendar-table tr td:first-child,  /deep/ .el-calendar-table__row td.prev{
+  border:none;
+ }
+.date-content {
+  height: 40px;
+  text-align: center;
+  line-height: 40px;
+  font-size: 14px;
+}
+.date-content .rest {
+  color: #fff;
+  border-radius: 50%;
+  background: rgb(250, 124, 77);
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+  display: inline-block;
+  font-size: 12px;
+  margin-left: 10px;
+}
+.date-content .text{
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+ display: inline-block;
+
+}
+ /deep/ .el-calendar-table td.is-selected .text{
+   background: #409eff;
+   color: #fff;
+   border-radius: 50%;
+ }
+ /deep/.el-calendar__header {
+   display: none
+ }
+</style>
